464898c5a295a0600fff082228edb941b89756f8
Yassine0x <yassine01bouziane@gmail.com>
2024-01-16 11:28:49 +0100

    add mxgraph editor

--- app/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var container = document.getElementById('graphContainer');
-    var graph = new mxGraph(container);
-
-    // Add a vertex
-    var parent = graph.getDefaultParent();
-    var vertex = graph.insertVertex(parent, null, "first test", 20, 20, 150, 30);
-});
